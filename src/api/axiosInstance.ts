82d1b6c3a14c91dce13a879e05eb4a591cbc40b0
Ajay Verma <aryanav30@gmail.com>
2026-02-21 02:32:42 +0530

    updated base url again

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://final-backend-production-c23e.up.railway.app',
+  baseURL: 'https://final-backend-production-8ed7.up.railway.app',
   withCredentials: true
 });
 
