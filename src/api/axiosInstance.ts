cac4eee187a2e86feccce3f186ec7210bcde5fcb
Ajay Verma <aryanav30@gmail.com>
2026-04-29 23:32:25 +0530

    Updated the URL

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://final-backend-production-b5b0.up.railway.app',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true
 });
 
